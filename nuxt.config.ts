aa2d97307798e1687e3539edfa40d4edbe5fdfe5
Admin <admin@example.com>
2026-02-05 14:59:53 +0900

    github 페이지 설정 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   telemetry: { enabled: false },
   app: {
+    baseURL: '/ai-fitting-admin/', // 반드시 레포지토리 이름과 일치해야 함
+    buildAssetsDir: 'assets',     // '_nuxt' 대신 'assets' 사용 (GitHub Pages의 지킬 무시 방지)
     head: {
       style: [
         {
@@ -36,6 +38,7 @@ export default defineNuxtConfig({
       ]
     }
   },
+  ssr: false, // 정적 호스팅이므로 클라이언트 사이드 렌더링(SPA) 모드로 설정
   alias: {
     'mnemonist/lru-cache': 'mnemonist/lru-cache.js'
   },
