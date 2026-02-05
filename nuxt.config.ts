f8747f0ad6994b17f8195074fcf2de9913ed56cd
EasyBAMM <huhjb1020@naver.com>
2026-02-05 15:49:58 +0900

    chore: HANDSOME 로고 파비콘 추가 및 설정 반영

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         // rel="icon" 설정을 통해 탭 아이콘 지정
         // baseURL이 적용되므로 '/favicon.ico'로 작성해도 실제로는 '/ai-fitting-admin/favicon.ico'를 참조합니다.
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ]
+      ],
       style: [
         {
           innerHTML: `
